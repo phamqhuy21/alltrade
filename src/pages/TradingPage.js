9502d3ad6055cc61199a3d226314a562ef8e8926
huyy <phamqhuy21>
2021-03-27 14:30:50 +0700

    feat(pages/TradingPage.js): add styles, update ui

--- src/pages/TradingPage.js
+++ src/pages/TradingPage.js
@@ -2,9 +2,23 @@ import routes from "../routes/tradingRoutes";
 import { Route } from "react-router-dom";
 import TopBar from "../components/TopBar/TopBar";
 import SideBar from "../components/SideBar/SideBar";
-import { Grid } from "@material-ui/core";
+import { createStyles, Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(
+  () =>
+    createStyles({
+      root: {
+        backgroundColor: "#0c0c0c",
+        color: "#fff",
+      },
+      body: { width: "94%" },
+    }),
+  { index: 1 }
+);
 
 export default function TradingPage() {
+  const classes = useStyles();
+
   const showBody = (routes) => {
     let result = null;
     if (routes.length > 0) {
@@ -23,15 +37,13 @@ export default function TradingPage() {
   };
 
   return (
-    <div>
-      <TopBar />
+    <div className={classes.root}>
       <Grid container spacing={1}>
-        <Grid item xs={1}>
-          <SideBar />
-        </Grid>
-        <Grid item xs={11}>
-          {showBody(routes)}
-        </Grid>
+        <TopBar />
+      </Grid>
+      <Grid container spacing={1}>
+        <SideBar />
+        <div className={classes.body}>{showBody(routes)}</div>
       </Grid>
       {/* <div style={{ display: "flex" }}>
         <div>
